9d9e090260e3f0be27c10c2a2359db7408b36e35
stefanzweifel <stefanzweifel@users.noreply.github.com>
2022-02-06 16:23:59 +0000

    Fix styling

--- tests/BrowsershotLambdaTest.php
+++ tests/BrowsershotLambdaTest.php
@@ -1,25 +1,19 @@
 <?php
 
 it('generates screenshot from url', function () {
-
 })->skip();
 
 it('generates screenshot from html', function () {
-
 })->skip();
 
 it('generates pdf from url', function () {
-
 })->skip();
 
 it('generates pdf from html', function () {
-
 })->skip();
 
 it('returns raw html from url', function () {
-
 })->skip();
 
 it('returns raw html from html', function () {
-
 })->skip();
