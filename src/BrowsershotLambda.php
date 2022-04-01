4c69dc2c8348e303bd7d16f5f25438361638a319
Mark Salmon <mark.salmon@mediconesystems.com>
2022-04-01 23:36:08 +0100

    save to S3

--- src/BrowsershotLambda.php
+++ src/BrowsershotLambda.php
@@ -6,6 +6,7 @@ use Hammerstone\Sidecar\Results\SettledResult;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 use Spatie\Browsershot\Browsershot;
+use Spatie\Browsershot\Exceptions\CouldNotTakeBrowsershot;
 use Spatie\Browsershot\Exceptions\ElementNotFound;
 use Wnx\SidecarBrowsershot\Functions\BrowsershotFunction;
 
@@ -28,8 +29,9 @@ class BrowsershotLambda extends Browsershot
         }
 
         $path = Arr::get($command, 'options.path');
+        $s3 = Arr::get($command, 'options.path');
 
-        if ($path) {
+        if ($path && !$s3) {
             file_put_contents($path, base64_decode($response->body()));
             return $path;
         } else {
@@ -52,5 +54,33 @@ class BrowsershotLambda extends Browsershot
         }
 
         $response->throw();
+    }
+
+    /**
+     * @throws CouldNotTakeBrowsershot
+     */
+    protected function saveToS3(string $targetPath, string $disk = 's3')
+    {
+        $this->setOption('s3', [
+            'path' => $targetPath,
+            'region' => config('sidecar.region'),
+            'bucket' => config("filesystems.disks.$disk.bucket")
+        ]);
+
+        $extension = strtolower(pathinfo($targetPath, PATHINFO_EXTENSION));
+
+        if ($extension === '') {
+            throw CouldNotTakeBrowsershot::outputFileDidNotHaveAnExtension($targetPath);
+        }
+
+        $command = $extension === 'pdf'
+            ? $this->createPdfCommand($targetPath)
+            : $this->createScreenshotCommand($targetPath);
+
+        $result = $this->callBrowser($command);
+
+        if (empty($result['ETag'])) {
+            throw CouldNotTakeBrowsershot::chromeOutputEmpty("$targetPath on S3 disk: $disk", $command);
+        }
     }
 }
