f3b9a80d6e74d684ca8f146e1bb1b9d7cde3903b
Stefan Zweifel <hello@stefanzweifel.io>
2022-05-28 16:40:47 +0200

    Add sidecar-browsershot config file

---
+++ config/sidecar-browsershot.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'memory' => env('SIDECAR_BROWSERSHOT_MEMORY', 2048),
+
+    'warming' => env('SIDECAR_BROWSERSHOT_WARMING_INSTANCES', 0),
+
+    'layer' => env('SIDECAR_BROWSERSHOT_LAYER'),
+];
